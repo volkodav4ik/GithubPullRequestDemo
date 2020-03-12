8968d6904c2cbcb01c8358d975bbcfd19b77a9f3
Artem Vovk <volkodav4ik1990@gmail.com>
2020-03-12 22:11:09 +0200

    Add useless stream.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
+    private static final int SOME_NUMBER = 1000;
+    private static final int MIN_VALUE = 200;
+    private static final int MAX_VALUE = 400;
 
     public static void main(String[] args) {
-        // show me you Stream API power
-        System.out.println("test");
+
+        Random random = new Random();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < SOME_NUMBER; i++) {
+            list.add(Integer.toString(random.nextInt(SOME_NUMBER)));
+        }
+
+        int[] array = list.stream()
+                .mapToInt(Integer::parseInt)
+                .filter((i) -> (i >= MIN_VALUE && i <= MAX_VALUE))
+                .distinct()
+                .sorted()
+                .toArray();
+        System.out.println(Arrays.toString(array));
     }
 
 }
